e8343fa0bc004595401e8ff09ca976335444fa7c
unknown <16nitink7@gmail.com>
2021-04-08 21:17:44 +0530

    Added Cart Files

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,10 @@ import { ReactComponent as Logo} from '../../assets/crown.svg'
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+
+const Header = ({ currentUser, hidden }) => (
 	<div className="header">
 		<Link to="/" className="logo-container">
 			<Logo className="logo" />
@@ -29,13 +32,17 @@ const Header = ({ currentUser }) => (
 					SIGN IN
 				</Link>
 			}
-			
+			<CartIcon />
 		</div>
+		{
+			hidden ? null : <CartDropdown />
+		}
 	</div>
 )
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: { hidden }}) => ({
+	currentUser,
+	hidden
 })
 
 export default connect(mapStateToProps)(Header)
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,9 +6,6 @@ import Directory from "../../components/directory/directory.component"
 
 const HomePage = () => (
 	<div className="homepage">
-		<center>
-			<h1>KING CLOTHHOUSE</h1>
-		</center>
 		<Directory />
 	</div>
 )
